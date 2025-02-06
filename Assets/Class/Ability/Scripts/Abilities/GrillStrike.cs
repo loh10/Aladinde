1ce2bb323e4dc3e23585569271e1c95e328e78fd
MattC-bit <58609954+MattC-bit@users.noreply.github.com>
2025-02-06 17:22:45 +0100

    Add Leaf Throw + SpiceJet

--- Assets/Class/Ability/Scripts/Abilities/GrillStrike.cs
+++ Assets/Class/Ability/Scripts/Abilities/GrillStrike.cs
@@ -11,19 +11,26 @@ public class GrillStrike : Ability
 
     public override void Activate(GameObject user)
     {
+        // Execute common ability logic (e.g., charge management)
         base.Activate(user);
         
+        // Convert mouse position to world coordinates
         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        // Calculate the normalized direction from the user to the mouse
         Vector2 direction = (mousePos - (Vector2)user.transform.position).normalized;
+        
+        // Cast a ray from the user's position in the calculated direction up to the defined range
         RaycastHit2D hit = Physics2D.Raycast(user.transform.position, direction, range);
-        if (hit)
+        
+        // If the ray hits an object that is not the user
+        if (hit && hit.collider.gameObject != user)
         {
-            if (hit.collider.gameObject != user)
-            {
-                user.GetComponent<PlayerLifeManager>().TakeDamageServerRpc(damages, hit.collider.GetComponent<NetworkObject>().OwnerClientId);
-            }
+            // Apply damage to the hit target using its NetworkObject's OwnerClientId
+            user.GetComponent<PlayerLifeManager>().TakeDamageServerRpc(damages, hit.collider.GetComponent<NetworkObject>().OwnerClientId);
         }
         
+        // Log that the ability has been activated
         Debug.Log(abilityName + " activated");
     }
 
