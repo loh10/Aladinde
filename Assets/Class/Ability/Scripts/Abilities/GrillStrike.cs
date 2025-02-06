e6bedb88b36c47dcb2078724da2d214e43d5682c
MattC-bit <58609954+MattC-bit@users.noreply.github.com>
2025-02-06 17:09:34 +0100

    Add GrillStrike basic implementation

--- Assets/Class/Ability/Scripts/Abilities/GrillStrike.cs
+++ Assets/Class/Ability/Scripts/Abilities/GrillStrike.cs
@@ -12,6 +12,18 @@ public class GrillStrike : Ability
     public override void Activate(GameObject user)
     {
         base.Activate(user);
+        
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePos - (Vector2)user.transform.position).normalized;
+        RaycastHit2D hit = Physics2D.Raycast(user.transform.position, direction, range);
+        if (hit)
+        {
+            if (hit.collider.gameObject != user)
+            {
+                user.GetComponent<PlayerLifeManager>().TakeDamageServerRpc(damages, hit.collider.GetComponent<NetworkObject>().OwnerClientId);
+            }
+        }
+        
         Debug.Log(abilityName + " activated");
     }
 
