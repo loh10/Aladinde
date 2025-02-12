4599cfcb456949f1d04a7e4ff658c4d2fa43afc6
Loh10 <naturellement.toi@gmail.com>
2025-02-12 17:18:29 +0100

    fix bots attack

--- Assets/Player/Scripts/PlayerLifeManager.cs
+++ Assets/Player/Scripts/PlayerLifeManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
 
 
 public class PlayerLifeManager : NetworkBehaviour
@@ -31,10 +32,12 @@ public class PlayerLifeManager : NetworkBehaviour
         _shieldBar.maxValue = _maxShield;
         _shieldBar.value = 0;
 
+        _currentHealth = new NetworkVariable<float>(_maxHealth);
+        Debug.Log(_currentHealth.Value + " On this target " + gameObject.name);
         // Only the server should initialize the NetworkVariables.
         if (IsServer)
         {
-            _currentHealth = new NetworkVariable<float>(_maxHealth);
+
             _currentShield.Value = 0;
         }
         
@@ -109,7 +112,7 @@ public class PlayerLifeManager : NetworkBehaviour
         }
     }
 
-    [ServerRpc]
+    [ServerRpc(RequireOwnership = false)]
     public void TakeDamageServerRpc(float damage, ulong targetClientId)
     {
         PlayerLifeManager[] players = FindObjectsByType<PlayerLifeManager>(FindObjectsInactive.Include, FindObjectsSortMode.None);
