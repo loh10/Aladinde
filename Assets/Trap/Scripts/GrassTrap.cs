d0191f1b7c6fcc70d3f68acafdf1e9779ba36a5e
Adahaut <adao.vo.t@gmail.com>
2025-02-12 18:59:03 +0100

    disappear in bush

--- Assets/Trap/Scripts/GrassTrap.cs
+++ Assets/Trap/Scripts/GrassTrap.cs
@@ -1,3 +1,4 @@
+using Unity.Netcode;
 using UnityEngine;
 
 public class GrassTrap : MonoBehaviour, ITrapEffect
@@ -5,13 +6,15 @@ public class GrassTrap : MonoBehaviour, ITrapEffect
     public void ApplyEffect(GameObject player)
     {
         SpriteRenderer sprite = player.GetComponent<SpriteRenderer>();
+        player.GetComponent<PlayerInfos>().canvas.SetActive(false);
         if (sprite != null)
-            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0);
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 0f);
     }
 
     public void RemoveEffect(GameObject player)
     {
         SpriteRenderer sprite = player.GetComponent<SpriteRenderer>();
+        player.GetComponent<PlayerInfos>().canvas.SetActive(true);
         if (sprite != null)
             sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, 1);
     }
